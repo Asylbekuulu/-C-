8a90b23a8443c42a8e5768b82fed2885a388027e
Erjan <erzhanasylbekuulu@gmail.com>
2023-11-25 15:26:18 +0600

    создание проекта с# Hello world

---
+++ examle001_Helloworld/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! erjan");
+// dotnet new console - создать новый проект
+// dotnet run - запустить проект
+// Через консоль мы обращяемся к системному терминалу
